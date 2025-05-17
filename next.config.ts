a07c2132e3df5609b98f98826041021dffc5c6d7
Jesse Miller <silverhonda84@gmail.com>
2025-05-17 14:37:08 -0700

    added ignore during builds

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
-
+ 
 const nextConfig: NextConfig = {
   images: {
     domains: ['m.media-amazon.com'],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
